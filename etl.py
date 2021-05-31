0bad51e748e036612f1cf5b17a7134750938bb9a
yasar1998 <31879611+yasar1998@users.noreply.github.com>
2021-05-31 03:00:12 +0200

    Add files via upload

---
+++ etl.py
@@ -0,0 +1,36 @@
+import pandas as pd
+
+extract_csv = pd.read_csv('resources/extract.csv')
+extract_json = pd.read_json('resources/extract.json')
+
+
+def mergeDataSets(dataSet1, dataSet2):
+    dataSet = dataSet1.append(dataSet2, ignore_index=True)
+    return dataSet
+
+
+def removeNullValues(dataSet):
+    null_removed = dataSet.dropna()
+    null_removed = null_removed.reset_index(drop=True)
+    return null_removed
+
+
+def removeDuplicates(dataSet):
+    dataSet.drop_duplicates(inplace=True)
+    duplicates_removed = dataSet.reset_index(drop=True)
+    return duplicates_removed
+
+
+def calculateBMI(weight, height):
+    return weight / (height ** 2)
+
+
+extract = mergeDataSets(extract_csv, extract_json)
+extract = removeNullValues(extract)
+extract = removeDuplicates(extract)
+extract['body_mass_index'] = calculateBMI(extract['weight'], extract['height'])
+
+extract.to_csv('resources/load.csv')
+extract.to_json('resources/load.json')
+
+print(extract)
